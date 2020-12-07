94ba0cdc86a82e846eb248aead4e1c6e0dfae361
Renaud Martinet <me@renaudmarti.net>
2020-12-07 14:16:47 +0100

    Clean up test template

--- test_template.py
+++ test_template.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-import unittest
-import main
 import io
+import main
+import unittest
 
 class TestMain(unittest.TestCase):
     data = """
@@ -10,9 +10,9 @@ class TestMain(unittest.TestCase):
 
     def test_part_one(self):
         data = main.parse(io.StringIO(self.data))
-        assert main.part_one(data) == 1337
+        self.assertEqual(main.part_one(data), 1337)
 
     def test_part_two(self):
         data = main.parse(io.StringIO(self.data))
-        assert main.part_two(data) == 42
+        self.assertEqual(main.part_two(data), 42)
 
