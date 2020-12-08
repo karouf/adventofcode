2d8abbe42c38cfcb9ecf57c1aa2e75cf0f13e4b5
Renaud Martinet <me@renaudmarti.net>
2020-12-08 14:00:47 +0100

    Day 8 part 2

--- 2020/8/main.py
+++ 2020/8/main.py
@@ -27,20 +27,74 @@ def run(code, ip, acc):
     elif op == 'jmp':
         return ip + int(val), acc
 
+def detect_loop(code, start, loops):
+    executed = set()
+    next_ip = start
+    acc = 0
+    while True:
+        if next_ip in executed:
+            return True
+
+        if next_ip == len(code):
+            return False
+
+        if loops[next_ip] is not None:
+            return loops[next_ip]
+
+        executed.add(next_ip)
+        next_ip, acc = run(code, next_ip, acc)
+
+
+def detect_loops(code):
+    loops = [None] * len(code)
+    i = len(code) - 1
+    while i >= 0:
+        loops[i] = detect_loop(code, i, loops)
+        i -= 1
+    return loops
+
+
+def fix_code(code):
+    loops = detect_loops(code)
+
+    executed = set()
+    next_ip = 0
+    acc = 0
+    while True:
+        executed.add(next_ip)
+        next_ip, acc = run(code, next_ip, acc)
+        if next_ip in executed:
+            break
+
+    for ip in executed:
+        op, val = code[ip]
+        if op == 'nop':
+            next_ip = ip + int(val)
+            if not loops[next_ip]:
+                code[ip][0] = 'jmp'
+                return code
+        elif op == 'jmp':
+            next_ip = ip + 1
+            if not loops[next_ip]:
+                code[ip][0] = 'nop'
+                return code
+        else:
+            continue
+
 
 def part_one(data):
     executed = set()
-    next = 0
+    next_ip = 0
     acc = 0
     while True:
-        executed.add(next)
-        next, acc = run(data, next, acc)
-        if next in executed:
+        executed.add(next_ip)
+        next_ip, acc = run(data, next_ip, acc)
+        if next_ip in executed or next_ip == len(data):
             return acc
 
 
 def part_two(data):
-    pass
+    return part_one(fix_code(data))
 
 
 if __name__ == '__main__':
--- 2020/8/test_main.py
+++ 2020/8/test_main.py
@@ -20,10 +20,9 @@ acc +6"""
         data = main.parse(io.StringIO(self.data))
         self.assertEqual(main.part_one(data), 5)
 
-    @unittest.skip('Wait for part one to be done')
     def test_part_two(self):
         data = main.parse(io.StringIO(self.data))
-        self.assertEqual(main.part_two(data), 42)
+        self.assertEqual(main.part_two(data), 8)
 
     def test_part_one_for_real(self):
         input_path = os.path.join(os.path.dirname(__file__), 'input.data')
@@ -31,9 +30,8 @@ acc +6"""
             data = main.parse(input_file)
         self.assertEqual(main.part_one(data), 1782)
 
-    @unittest.skip('Wait for part two to be done')
     def test_part_two_for_real(self):
         input_path = os.path.join(os.path.dirname(__file__), 'input.data')
         with open(input_path) as input_file:
             data = main.parse(input_file)
-        self.assertEqual(main.part_two(data), 23)
+        self.assertEqual(main.part_two(data), 797)
